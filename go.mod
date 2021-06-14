94eaaed7ecac4baf0a92464f4cae4733a9d3a5f1
arkssss <522500442@qq.com>
2021-06-14 18:00:54 +0800

    change pb version

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/code-newbee/grpc-gateway
 go 1.14
 
 require (
-	github.com/code-newbee/protocol/geeker v0.0.0-20210613103205-5c5b4fc7630c
+	github.com/code-newbee/protocol/geeker v0.0.0-20210614091437-083cd3912559
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	golang.org/x/net v0.0.0-20210610132358-84b48f89b13b // indirect
 	golang.org/x/sys v0.0.0-20210611083646-a4fc73990273 // indirect
--- internal/main.go
+++ internal/main.go
@@ -9,26 +9,26 @@ import (
 	"net/http"
 )
 
-const  (
+const (
 	geekerEndPoint = "localhost:50001"
 )
 
-func Run()  {
+func Run() {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
 	defer cancel()
 
 	mux := runtime.NewServeMux()
-	err := gw.RegisterGeekerHandlerFromEndpoint(ctx, mux, geekerEndPoint, []grpc.DialOption{
+	err := gw.RegisterGreekerHandlerFromEndpoint(ctx, mux, geekerEndPoint, []grpc.DialOption{
 		grpc.WithInsecure(),
 	})
 
-	if err != nil{
+	if err != nil {
 		log.Fatal("fail to register endpoint")
 	}
 
-	if err := http.ListenAndServe(":8081", mux); err != nil{
+	if err := http.ListenAndServe(":8081", mux); err != nil {
 		log.Fatalf("fail to setup http svr")
 	}
 }
